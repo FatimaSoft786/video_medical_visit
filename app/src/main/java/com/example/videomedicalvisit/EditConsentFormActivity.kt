0de05c14b6d962ed25125773597c2bdb2839ca1e
shahzaib65 <34715079+shahzaib65@users.noreply.github.com>
2024-09-13 23:37:57 +0500

    Doctor and other screens are ready.

---
+++ app/src/main/java/com/example/videomedicalvisit/EditConsentFormActivity.kt
@@ -0,0 +1,14 @@
+package com.example.videomedicalvisit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EditConsentFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_consent_form)
+    }
+}
